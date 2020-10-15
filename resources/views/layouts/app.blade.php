f9b751a84f8b3832d4c018c0b2407f666e1df899
guzuri-nu <qpmz.wonx67@gmail.com>
2020-10-15 06:44:20 +0000

    user login

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,9 +19,21 @@
 
                 <div class="collapse navbar-collapse" id="nav-bar">
                     <ul class="navbar-nav mr-auto"></ul>
+                    <ul class="nav navbar-nav">
+                        @if (Auth::check())
+                             {{-- 作成ページへのリンク --}}
+                            <li class="nav-item">{!! link_to_route('tasks.create', '新規タスクの登録', [], ['class' => 'nav-link']) !!}</li>                        
+                            {{-- ログアウトへのリンク --}}
+                            <li class="nav-item">{!! link_to_route('logout.get', 'Logout', [], ['class' => 'nav-link']) !!}</li>
+                        @else
+                             {{-- ユーザ登録ページへのリンク --}}
+                            <li class ="nav-item">{!! link_to_route('signup.get', 'Signup', [], ['class' => 'nav-link']) !!}</li>
+                            {{-- ログインページへのリンク --}}
+                            <li class="nav-item">{!! link_to_route('login', 'Login', [], ['class' => 'nav-link']) !!}</li>
+                        @endif  
+                </ul>
                     <ul class="navbar-nav">
-                         {{-- 作成ページへのリンク --}}
-                <li class="nav-item">{!! link_to_route('tasks.create', '新規タスクの登録', [], ['class' => 'nav-link']) !!}</li>
+                       
                     </ul>
                 </div>
             </nav>
